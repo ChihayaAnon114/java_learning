8a00bc7ea543120973228445d32d51ef3f63403e
千早爱音 <sht1953893096@gmail.com>
2025-12-25 10:45:06 +0800

    2222

--- src/demo1.java
+++ src/demo1.java
@@ -1,7 +1,7 @@
 public class demo1 {
     public static void a1()
     {
-        System.out.println("1111");
+        System.out.println("2222");
     }
 
     public static void main(String[] args) {

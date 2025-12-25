d41f927a4eded6b93dda23d6949598b619f1fa4e
千早爱音 <sht1953893096@gmail.com>
2025-12-25 10:33:51 +0800

    1

--- src/demo1.java
+++ src/demo1.java
@@ -1,7 +1,10 @@
 public class demo1 {
-    public static void
+    public static void a1()
+    {
+        System.out.println("1");
+    }
 
     public static void main(String[] args) {
-
+        a1();
     }
 }

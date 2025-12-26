2c044fc414d77cca62ac593f307bcd523d28ee23
千早爱音 <sht1953893096@gmail.com>
2025-12-27 00:25:46 +0800

    java练习题

---
+++ src/d6/Student.java
@@ -0,0 +1,26 @@
+package d6;
+
+import java.util.Date;
+
+public class Student {
+    /*
+    2．创建类Student，包含成员如下
+    a)private属性：name、sex、birthday（该属性要求为Date类型）
+    b)默认构造方法，为属性初始化为：“新同学”，性别为“未知”，生日为“1990-4-10”
+    c)带有5个参数的构造方法，根据参数为属性赋值
+    d)public void showMessage()，输出学生的所有信息。
+     */
+    private String name;
+    private String gender;
+    private M_date birthday;
+    Student(){
+        M_date default_birthday=new M_date(1990,4,10);
+        this.name="新同学";this.gender="未知";this.birthday=default_birthday;
+    }
+    Student(String n,String g,int y,int m,int d){
+        this.name=n;this.gender=g;this.birthday=new M_date(y,m,d);
+    }
+    public void show(){
+        System.out.println("student message: name:"+this.name+" gender:"+this.gender+" birthday:"+birthday.showDate());
+    }
+}
---
+++ src/d6/t5.java
@@ -0,0 +1,41 @@
+package d6;
+import java.util.Scanner;
+/*
+1．创建类Date，用来描述日期数据，包含成员如下
+a)private属性year、month、day，分别用来表示日期的年、月、日
+b)默认构造方法，给属性赋值为当前日期（根据今天的日期赋值即可）
+c)带参数的构造方法，根据参数给属性赋值
+d)public String showDate()：以字符串的方式返回由3个属性值组成的一个日期数据，年，月，日使用“-”间隔
+2．创建类Student，包含成员如下
+a)private属性：name、sex、birthday（该属性要求为Date类型）
+b)默认构造方法，为属性初始化为：“新同学”，性别为“未知”，生日为“1990-4-10”
+c)带有5个参数的构造方法，根据参数为属性赋值
+d)public void showMessage()，输出学生的所有信息。
+ */
+public class t5 {
+    /*
+    3．创建类TestStudent，包含main()，完成如下功能
+    a)创建一个Student类对象，调用默认构造方法为属性初始化，显示出该学生的信息，显示效果为：
+    学生***，性别为男，出生日期是：1990-4-10
+
+    创建类TestStudent，创建一个Student对象，调用带参数的构造方法为属性赋值，
+    传递的参数通过控制台输入获取，要求控台输入学生的名字、性别、年、月、日。显示出该学生的信息
+     */
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        Student stu1=new Student();
+        System.out.println("name:");
+        String n=inp.next();
+        System.out.println("gender:");
+        String g=inp.next();
+        System.out.println("birthdate:year");
+        int y= inp.nextInt();
+        System.out.println("birthdate:month");
+        int m= inp.nextInt();
+        System.out.println("birthdate:day");
+        int d= inp.nextInt();
+        Student stu2=new Student(n,g,y,m,d);
+        stu1.show();
+        stu2.show();
+    }
+}

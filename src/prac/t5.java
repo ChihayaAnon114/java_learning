90bb14c262d266f63814bde0c257e8d62e365a8d
千早爱音 <sht1953893096@gmail.com>
2026-01-02 18:37:12 +0800

    Merge remote-tracking branch 'origin/master'

--- src/prac/t5.java
+++ src/prac/t5.java
@@ -1,4 +1,23 @@
 package prac;
-
+import java.util.ArrayList;
+import java.util.List;
 public class t5 {
+    //输入一个链表，从尾到头打印链表每个节点的值。
+    public static void main(String[] args) {
+        //......
+        ArrayList lst=new ArrayList<Integer>();
+        for (int i = 100; i >0; i--) {
+            lst.add(i);
+        }
+        System.out.println(lst);
+        System.out.println(reverse_l(lst));
+    }
+
+    public static ArrayList reverse_l(ArrayList list){
+        ArrayList temp_lst =new ArrayList<>();
+        for (int i = 0; i <list.size() ; i++) {
+            temp_lst.add(list.get(list.size()-i-1));
+        }
+        return temp_lst;
+    }
 }

6b5e581ae538c9a6e90088d9a4b4e2f40f39b79d
千早爱音 <sht1953893096@gmail.com>
2026-01-09 20:49:16 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/d14/newthread.java
@@ -0,0 +1,10 @@
+package d14;
+
+public class newthread extends Thread{
+    //一个进程
+    //基于runnable实现的
+    public static void main(String[] args) {
+        //do something...
+        System.out.println("do something...");
+    }
+}
